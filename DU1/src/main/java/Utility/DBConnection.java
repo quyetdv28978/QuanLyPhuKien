02519a9ce77671f1bae5382b0b8a8b5f9d5ed7e4
quyetdv28978 <107168139+quyetdv28978@users.noreply.github.com>
2023-03-24 01:55:26 +0700

    no message

--- DU1/src/main/java/Utility/DBConnection.java
+++ DU1/src/main/java/Utility/DBConnection.java
@@ -1,5 +1,22 @@
-package Utilities;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+// <<<<<<< HEAD
+package utility;
 
+//import DomainModel.ChucVu;
+//import DomainModel.NhanVien;
+//import ViewModel.NhanVienView;
+// import domaiModel.ChatLieu;
+// import domaiModel.DanhMuc;
+// import domaiModel.SanPham;
+// =======
+// package Utility;
+
+// import DomainModel.ChucVu;
+// import DomainModel.NhanVien;
+// >>>>>>> Kanna
 import java.util.List;
 import java.util.Properties;
 import org.hibernate.Session;
@@ -9,41 +26,51 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
+/**
+ *
+ * @author yugip
+ */
 public class DBConnection {
 
     private static final SessionFactory FACTORY;
 
     static {
-        Configuration conf = new Configuration();
+        Configuration confi = new Configuration();
         Properties pro = new Properties();
         pro.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
         pro.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
         pro.put(Environment.URL, "jdbc:sqlserver://localhost;database=DU1_NHOM1;trustServerCertificate=true");
-        pro.put(Environment.USER, "sa"); 
-        pro.put(Environment.PASS, "123456");
-
-        pro.put(Environment.PASS, "123");
-
+// <<<<<<< HEAD
+        pro.put(Environment.USER, "sa");
+        pro.put(Environment.PASS, "2132411");
         pro.put(Environment.SHOW_SQL, true);
-        conf.setProperties(pro);
+        confi.setProperties(pro);
+        confi.addAnnotatedClass(ChatLieu.class);
+        confi.addAnnotatedClass(DanhMuc.class);
+        confi.addAnnotatedClass(SanPham.class);
+// =======
+//         pro.put(Environment.USER, "Kanna");
+//         pro.put(Environment.PASS, "S2gaungok");
+//         pro.put(Environment.SHOW_SQL, true);
+//         confi.setProperties(pro);
+//         confi.addAnnotatedClass(ChucVu.class);
+//         confi.addAnnotatedClass(NhanVien.class);
+// >>>>>>> Kanna
 
-        ServiceRegistry ser = new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
-        FACTORY = conf.buildSessionFactory(ser);
+        ServiceRegistry ser = new StandardServiceRegistryBuilder().applySettings(confi.getProperties()).build();
+        FACTORY = confi.buildSessionFactory(ser);
+
     }
 
-    public static SessionFactory getsetFactory() {
+    public static SessionFactory getseFactory() {
         return FACTORY;
     }
-
-    ;
-
-
 
     public static List selectQueRy(String sql) {
         Session ss = null;
         List listOB = null;
         try {
-            ss = getsetFactory().openSession();
+            ss = getseFactory().openSession();
             listOB = ss.createQuery(sql).getResultList();
         } catch (Exception e) {
             e.printStackTrace();
@@ -58,7 +85,7 @@ public class DBConnection {
         Session ss = null;
         List listOB = null;
         try {
-            ss = getsetFactory().openSession();
+            ss = getseFactory().openSession();
             listOB = ss.createQuery(sql).getResultList();
         } catch (Exception e) {
             e.printStackTrace();
@@ -72,7 +99,7 @@ public class DBConnection {
     public static int executeQuery(Object o, String id) {
         Session ss = null;
         try {
-            ss = getsetFactory().openSession();
+            ss = getseFactory().openSession();
             if (id != null) {
                 ss.getTransaction().begin();
                 ss.update(o);
@@ -94,7 +121,7 @@ public class DBConnection {
     public static int delete(String id, Class a) {
         Session ss = null;
         try {
-            ss = getsetFactory().openSession();
+            ss = getseFactory().openSession();
             Object o = ss.get(a, id);
             ss.getTransaction().begin();
             ss.delete(o);
