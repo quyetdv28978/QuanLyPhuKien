2d86e969dfc12c34c571450eca4d4724642c6c00
Nguyenhuong011203 <nguyenthihuong011203@gmail.com>
2023-04-04 00:12:22 +0700

    no message

--- DU1/src/main/java/utility/DBConnection.java
+++ DU1/src/main/java/utility/DBConnection.java
@@ -54,9 +54,9 @@ public class DBConnection {
         pro.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
         pro.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
 //        pro.put(Environment.URL, "jdbc:sqlserver://localhost;database=DU1_NHOM;trustServerCertificate=true");
-        pro.put(Environment.URL, "jdbc:sqlserver://localhost;database=DU1_NHOM1_1_2;trustServerCertificate=true");
+        pro.put(Environment.URL, "jdbc:sqlserver://localhost;database=DU1_NHOM1_1;trustServerCertificate=true");
         pro.put(Environment.USER, "sa");
-        pro.put(Environment.PASS, "2132411");
+        pro.put(Environment.PASS, "123456");
         pro.put(Environment.SHOW_SQL, true);
         confi.setProperties(pro);
 
